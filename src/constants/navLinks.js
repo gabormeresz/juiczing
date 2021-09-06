aa6d82d473a4ebc17c9be3150fe2626fe4c1cbbf
Gábor Merész <39135573+gabormeresz@users.noreply.github.com>
2021-09-06 16:05:12 +0200

    deleted page data: about

--- src/constants/navLinks.js
+++ src/constants/navLinks.js
@@ -2,7 +2,6 @@ const navLinks = [
   { path: "/", title: "home" },
   { path: "/recipes", title: "recipes" },
   { path: "/diets", title: "diets" },
-  { path: "/about", title: "about" },
   { path: "/contact", title: "contact" },
 ];
 
