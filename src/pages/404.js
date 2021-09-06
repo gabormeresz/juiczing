10d0ff84a7b327b7e3ef069940d346bab32df4cc
Gábor Merész <39135573+gabormeresz@users.noreply.github.com>
2021-09-06 17:00:12 +0200

    completed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,61 @@
 import React from "react";
+import styled from "styled-components";
+import { StaticImage } from "gatsby-plugin-image";
 
 import Seo from "../components/Seo";
 
 const NotFoundPage = () => (
-  <>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
+  <Wrapper>
+    <Seo title="Error" />
+    <div className="container">
+      <StaticImage
+        className="image"
+        src="../../assets/images/juiczing_home_1.png"
+        alt="fruits in glasses"
+        placeholder="blurred"
+        layout="fullWidth"
+      />
+      <div className="overlay"></div>
+      <div className="container-text">
+        <h1>404</h1>
+        <h4>Page not found</h4>
+      </div>
+    </div>
+  </Wrapper>
 );
 
 export default NotFoundPage;
+
+const Wrapper = styled.div`
+  .container {
+    display: grid;
+    height: calc(100vh - 5rem);
+    position: relative;
+  }
+  .image {
+    grid-area: 1/1;
+  }
+  .container-text {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  h1,
+  h4 {
+    color: var(--color-primary-6);
+    text-shadow: var(--shadow-dark-intensive);
+  }
+  h1 {
+    font-size: 5rem;
+  }
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.25);
+  }
+`;
