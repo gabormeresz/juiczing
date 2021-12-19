888314d30049f400762cf03da4ae432f3574da15
Gábor Merész <39135573+gabormeresz@users.noreply.github.com>
2021-12-19 12:23:07 +0100

    404 page modified

--- src/pages/404.js
+++ src/pages/404.js
@@ -18,7 +18,7 @@ const NotFoundPage = () => (
       <div className="overlay"></div>
       <div className="text">
         <h1>404</h1>
-        <h4>Page not found</h4>
+        <h4>Page not found!</h4>
       </div>
     </div>
   </Wrapper>
